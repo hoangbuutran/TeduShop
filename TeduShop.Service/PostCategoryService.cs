284dc84b8049aef6a838b6d09d2a79c8821bc600
hoangbuutran <33544451+hoangbuutran@users.noreply.github.com>
2018-03-12 10:42:57 +0700

    5445

---
+++ TeduShop.Service/PostCategoryService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IPostCategoryService
+    {
+        void Add(PostCategory postCategory);
+        void Update(PostCategory postCategory);
+        void Delete(int id);
+        IEnumerable<PostCategory> GetAll();
+        IEnumerable<PostCategory> GetAll(string keyWord);
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+        PostCategory GetById(int id);
+        void SaveChanges();
+    }
+    class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void Add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll(new string[] { "Posts" });
+        }
+
+        public IEnumerable<PostCategory> GetAll(string keyWord)
+        {
+            if (!string.IsNullOrEmpty(keyWord))
+            {
+                return _postCategoryRepository.GetMulti(x => x.Status && x.Name.Contains(keyWord) || x.MetaDescription.Contains(keyWord));
+            }
+            else
+            {
+                return _postCategoryRepository.GetAll(new string[] { "Posts" });
+            }
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status == true && x.ParentID == parentId, new string[] { "Posts" });
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
